23034a20eb4c846400fc6330a680d1f9be20748d
ctarrington <arrington.ct@gmail.com>
2016-01-15 22:56:08 -0500

    basic implementation of Column Transposition cipher

--- EncryptionAlgorithms/spec/AncientEncryptionSpec.js
+++ EncryptionAlgorithms/spec/AncientEncryptionSpec.js
@@ -1,23 +1,73 @@
-describe("TranspositionCipher", function() {
+describe("Row/Column Transposition Cipher", function() {
 
     it("should produce the correct ciphertext when full matrix", function () {
-        var tc = createTranspositionCipher(5, 'R');
+        var tc = createRowColumnTranspositionCipher(5, 'R');
         expect(tc.encrypt('THISISASECRETMESSAGE')).toEqual('TSRSHAESISTASEMGICEE');
     });
 
     it("should produce the correct plaintext when full matrix", function () {
-        var tc = createTranspositionCipher(5, 'R');
+        var tc = createRowColumnTranspositionCipher(5, 'R');
         expect(tc.decrypt('TSRSHAESISTASEMGICEE')).toEqual('THISISASECRETMESSAGE');
     });
 
     it("should produce the correct ciphertext when not full matrix", function () {
-        var tc = createTranspositionCipher(5, 'R');
+        var tc = createRowColumnTranspositionCipher(5, 'R');
         expect(tc.encrypt('THISISASECRETMESSA')).toEqual('TSRSHAESISTASEMRICER');
     });
 
     it("should produce the correct plaintext when not full matrix", function () {
-        var tc = createTranspositionCipher(5, 'R');
+        var tc = createRowColumnTranspositionCipher(5, 'R');
         expect(tc.decrypt('TSRSHAESISTASEMRICER')).toEqual('THISISASECRETMESSARR');
+    });
+});
+
+describe("String key to numbers", function() {
+
+    it("should produce the correct numbers when there are no repeats", function () {
+        var numbers = convertStringKeyToNumbers('CARTS');
+        expect(numbers.join(',')).toEqual('1,0,2,4,3');
+    });
+
+    it("should produce the correct numbers when perfectly ordered", function () {
+        var numbers = convertStringKeyToNumbers('ABCDE');
+        expect(numbers.join(',')).toEqual('0,1,2,3,4');
+    });
+
+    it("should produce the correct numbers when perfectly reverse ordered", function () {
+        var numbers = convertStringKeyToNumbers('EDCBA');
+        expect(numbers.join(',')).toEqual('4,3,2,1,0');
+    });
+
+    it("should produce null when there are repeats", function () {
+        var numbers = convertStringKeyToNumbers('CARAS');
+        expect(numbers).toBeNull();
     });
 
 });
+
+describe("Column Transposition Cipher", function() {
+
+    var key = 'CARTS';
+
+    it("should produce the correct ciphertext when full matrix", function () {
+        var tc = createColumnTranspositionCipher(key, 'R');
+        expect(tc.encrypt('THISISASECRETMESSAGE')).toEqual('HTIISASSCEERTEMSSAEG');
+    });
+
+    it("should produce the correct plaintext when full matrix", function () {
+        var tc = createColumnTranspositionCipher(key, 'R');
+        expect(tc.decrypt('HTIISASSCEERTEMSSAEG')).toEqual('THISISASECRETMESSAGE');
+    });
+
+    it("should produce the correct ciphertext when not full matrix", function () {
+        var tc = createColumnTranspositionCipher(key, 'R');
+        expect(tc.encrypt('THISISASECRETMESSA')).toEqual('HTIISASSCEERTEMSSARR');
+    });
+
+    it("should produce the correct plaintext when not full matrix", function () {
+        var tc = createColumnTranspositionCipher(key, 'R');
+        expect(tc.decrypt('HTIISASSCEERTEMSSARR')).toEqual('THISISASECRETMESSARR');
+    });
+});
+
+
--- EncryptionAlgorithms/src/AncientEncryption.js
+++ EncryptionAlgorithms/src/AncientEncryption.js
@@ -1,4 +1,24 @@
-function createTranspositionCipher(cipherColumns, padCharacter)
+function pad(text, blockLength, padCharacter)
+{
+    var paddedText = text;
+
+    var hanging = text.length%blockLength;
+    if (hanging > 0) {
+        var padLength = blockLength-hanging;
+        for (var pad_ctr=0; pad_ctr<padLength;pad_ctr++)
+        {
+            paddedText = paddedText+padCharacter;
+        }
+    }
+
+    return paddedText;
+}
+
+/*
+ * Row / Column Transposition Cipher from:
+ * https://www.safaribooksonline.com/library/view/essential-algorithms-a/9781118612101/24_chapter16.html
+ */
+function createRowColumnTranspositionCipher(cipherColumns, padCharacter)
 {
 
     function transpose(text, rows, columns)
@@ -16,25 +36,9 @@ function createTranspositionCipher(cipherColumns, padCharacter)
         return transposed;
     }
 
-    function pad(text, blockLength)
-    {
-        var paddedText = text;
-
-        var hanging = text.length%blockLength;
-        if (hanging > 0) {
-            var padLength = blockLength-hanging;
-            for (var pad_ctr=0; pad_ctr<padLength;pad_ctr++)
-            {
-                paddedText = paddedText+padCharacter;
-            }
-        }
-
-        return paddedText;
-    }
-
     function encrypt(plaintext)
     {
-        plaintext = pad(plaintext, cipherColumns);
+        plaintext = pad(plaintext, cipherColumns, padCharacter);
         var columns = cipherColumns;
         var rows = plaintext.length / cipherColumns;
         return transpose(plaintext, rows, columns);
@@ -52,4 +56,88 @@ function createTranspositionCipher(cipherColumns, padCharacter)
         decrypt: decrypt
     }
 
+}
+
+function convertStringKeyToNumbers(keyString)
+{
+    var columns = [];
+    for (var col_ctr=0; col_ctr<keyString.length;col_ctr++)
+    {
+        columns.push({originalIndex: col_ctr, keyLetter: keyString.substring(col_ctr,col_ctr+1)});
+    }
+
+    columns.sort(function compare(a,b) {
+        if (a.keyLetter > b.keyLetter) {
+            return 1;
+        }
+        if (a.keyLetter < b.keyLetter) {
+            return -1;
+        }
+        return 0;
+    });
+
+    var ordering = [];
+    var unique_values = {};
+    for (var col_ctr=0; col_ctr<keyString.length;col_ctr++)
+    {
+        var originalIndex = columns[col_ctr].originalIndex;
+        ordering.push(originalIndex);
+
+        var originalValue = columns[col_ctr].keyLetter;
+        unique_values[originalValue] = 1;
+    }
+
+    if (Object.keys(unique_values).length === keyString.length) { return ordering; }
+
+    return null;
+}
+
+function createColumnTranspositionCipher(key, padCharacter)
+{
+    function encrypt(plaintext)
+    {
+        plaintext = pad(plaintext, key.length, padCharacter);
+        var columnIndexes = convertStringKeyToNumbers(key);
+
+        var columns = key.length;
+        var rows = plaintext.length / columns;
+
+        var encrypted = '';
+        for (var row_ctr=0; row_ctr<rows;row_ctr++)
+        {
+            for (var col_ctr=0; col_ctr<columns; col_ctr++)
+            {
+                var index = row_ctr*columns+columnIndexes[col_ctr];
+                encrypted = encrypted + plaintext.substring(index, index+1);
+            }
+        }
+
+        return encrypted;
+    }
+
+    function decrypt(plaintext)
+    {
+        plaintext = pad(plaintext, key.length, padCharacter);
+        var columnIndexes = convertStringKeyToNumbers(key);
+
+        var columns = key.length;
+        var rows = plaintext.length / columns;
+
+        var encrypted = '';
+        for (var row_ctr=0; row_ctr<rows;row_ctr++)
+        {
+            for (var col_ctr=0; col_ctr<columns; col_ctr++)
+            {
+                var index = row_ctr*columns+columnIndexes[col_ctr];
+                encrypted = encrypted + plaintext.substring(index, index+1);
+            }
+        }
+
+        return encrypted;
+    }
+
+    return {
+        encrypt: encrypt,
+        decrypt: decrypt
+    }
 }
